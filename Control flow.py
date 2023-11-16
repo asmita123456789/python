85293216cdd941e505f88d835fa8df0350b3a66c
asmita123456789 <149476780+asmita123456789@users.noreply.github.com>
2023-11-16 21:27:47 +0530

    Add files via upload

---
+++ Control flow.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+#1) If statement :- 
+
+x = 5
+if x >= 5:
+    print("x is greater than 4")
+
+
+# In[10]:
+
+
+age = 20 
+if age >= 50:
+    print("you are adult, you can vote")
+else:
+    print("you are child, you can't vote")
+
+
+# In[9]:
+
+
+# 2) If-else statement :-
+
+x = 3
+if x > 2:
+    print("x is positive")
+else:
+    print("x is negative")
+
+
+# In[15]:
+
+
+# 3) if-elif-else :
+
+x = 0 
+if x > 0:
+    print("x is positive")
+elif:
+    print("x is negative")
+else:
+        print("x is zero")
+
+
+# In[16]:
+
+
+# 4) Nested if-else statement 
+
+Fruit = input("Enter a fruit (apple,banana,ormango):")
+if fruit == "apple":
+    print("Apple are ususally red price 20rs. ")
+elif fruit == "banana":
+    print("Banana are typically yellow.")
+elif fruit == "mango":
+    color = input("Are the mangoes yellow or orange? ")
+if color == "red":
+    print("Red apple are sweet.")
+elif color == "black":
+    print("Black mangoes are tart.")
+else:
+    print("Invalid color.")
+else:
+    print("This fruit not available.")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
